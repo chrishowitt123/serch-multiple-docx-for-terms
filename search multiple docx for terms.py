1a016506fddea5c467139c5c93bb0a17d81a49bc
Chris Howitt <62956122+chrishowitt123@users.noreply.github.com>
2020-11-13 20:26:52 +0100

    Update search multiple docx for terms.py

--- search multiple docx for terms.py
+++ search multiple docx for terms.py
@@ -1,45 +1,164 @@
-import glob
-from pathlib import Path
-import os, sys
-from docx import *
-import docx2txt
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct  9 14:43:49 2020
+@author: chris
+"""
+import pandas as pd
+import tkinter
+from tkinter import filedialog
+from iteration_utilities import deepflatten
 from collections import Counter
+import os
+import docx2txt
+from nltk.tokenize import PunktSentenceTokenizer
+from nltk.tokenize import RegexpTokenizer
+import glob
 
-folder = r'C:\Users\chris\Documents\Transgola\Clients\PROJECTS\2020\375091020_TM_JTI\Translation\MY COPY\Perfectit\Final\Splits'
 
-orignalExt = '\*.docx'
+print( "\n")  
+print("Welcome to DOCX_RepStrings!")
+print( "\n")  
+print("One file or multiple files?")
+print( "\n") 
 
-orignalFiles = folder + orignalExt
+answer = input("One file or multiple files? 1 / M: ")
 
-sourceFiles = []
+if answer == 1:
 
-for filepath in glob.iglob(orignalFiles):
-    sourceFiles.append(filepath)
+    root = tkinter.Tk()
+    root.wm_withdraw() # this completely hides the root window
+    filename = filedialog.askopenfilename()
     
-newFileNames = []
-
-for f in sourceFiles:
-    base = os.path.basename(f)
-    os.path.splitext(base)
-    newFileName = os.path.splitext(base)[0]
-    newFileNames.append(newFileName)
+    fn = filename.split('.')[0] #seperate filename from extention for f-string output files
     
+    text = docx2txt.process(filename)
+    sent_tokenizer = PunktSentenceTokenizer(text)
+    sents = sent_tokenizer.tokenize(text)
     
-text = []
-
-for file in sourceFiles:
+    allTextList = []
     
-    text.append(docx2txt.process(file))
+    for sent in sents:
+        s = sent.split("\n")
+        allTextList.append(s)
     
-search = ['KAs' , 'CA']
-
-fnt = dict(zip(newFileNames, text))
-
-
-for s in search:
-    for k, v in fnt.items():
-        if s in v:
-            counts = v.count(s)
-            print( "\n")
-            print(k  + '   ------>   ' +  s + '   ------>   ' +  str(counts))
+    allTextFlat = list(deepflatten(allTextList, depth=1))
+    allTextFlat = [ x.replace('\t', '') for x in allTextFlat ]
+    allTextFlat = [x for x in allTextFlat if x.strip()] 
+    
+    repeaters  = Counter(allTextFlat)
+    
+    results = pd.DataFrame.from_dict(repeaters, orient='index')
+    results.columns = ['No. of instances']  
+    results = results[results['No. of instances']>1]
+    results.sort_values(by=['No. of instances'],ascending=False, inplace=True)
+    pd.set_option("display.max_rows", None, "display.max_columns", None)
+    
+    results.reset_index(inplace=True)
+    results = results.rename(columns = {'index':'String'})
+    
+    strings = results['String'].tolist()
+    insts = results['No. of instances'].tolist()
+    
+    dml = []
+    
+    for string, inst in zip(strings, insts):
+        string_word_count = len(string.split(' '))
+        dup_material = string_word_count * inst - string_word_count
+        dml.append(dup_material)
+    
+        word_tokenizer = RegexpTokenizer(r'\w+')
+        tokens = word_tokenizer.tokenize(text)
+        
+        rep_words = sum(dml)
+        doc_word_count = len(tokens)
+        perc_rep_mat = (rep_words / doc_word_count) * 100
+        rep_mat_statment = f'     {round(perc_rep_mat, 2)} % repeated material.'
+        results['%'] = pd.Series(rep_mat_statment)
+        
+        
+        
+        
+        os.path.dirname(os.path.abspath(filename))
+        results.to_excel(filename + '_DOCX_RepStrings.xlsx')
             
+        print("Results!")
+        print( "\n")
+        print(results[['String', 'No. of instances']])
+        print( "\n")
+        print(rep_mat_statment)  
+        print( "\n")
+        print("Your DOCX_RepStrings report was successfully created!")
+        
+else:
+    
+    root = tkinter.Tk()
+    root.wm_withdraw() # this completely hides the root window
+    folder = filedialog.askdirectory()
+    orignalExt = '\*.docx'
+    orignalFiles = folder + orignalExt
+    
+    sourceFiles = []
+    for filepath in glob.iglob(orignalFiles):
+        sourceFiles.append(filepath)
+        
+        for filename in sourceFiles:
+    
+            fn = filename.split('.')[0] #seperate filename from extention for f-string output files
+                
+            text = docx2txt.process(filename)
+            sent_tokenizer = PunktSentenceTokenizer(text)
+            sents = sent_tokenizer.tokenize(text)
+            
+            allTextList = []
+            
+            for sent in sents:
+                s = sent.split("\n")
+                allTextList.append(s)
+            
+            allTextFlat = list(deepflatten(allTextList, depth=1))
+            allTextFlat = [ x.replace('\t', '') for x in allTextFlat ]
+            allTextFlat = [x for x in allTextFlat if x.strip()] 
+            
+            repeaters  = Counter(allTextFlat)
+            
+            results = pd.DataFrame.from_dict(repeaters, orient='index')
+            results.columns = ['No. of instances']  
+            results = results[results['No. of instances']>1]
+            results.sort_values(by=['No. of instances'],ascending=False, inplace=True)
+            pd.set_option("display.max_rows", None, "display.max_columns", None)
+            
+            results.reset_index(inplace=True)
+            results = results.rename(columns = {'index':'String'})
+            
+            strings = results['String'].tolist()
+            insts = results['No. of instances'].tolist()
+            
+            dml = []
+            
+            for string, inst in zip(strings, insts):
+                string_word_count = len(string.split(' '))
+                dup_material = string_word_count * inst - string_word_count
+                dml.append(dup_material)
+            
+            word_tokenizer = RegexpTokenizer(r'\w+')
+            tokens = word_tokenizer.tokenize(text)
+            
+            rep_words = sum(dml)
+            doc_word_count = len(tokens)
+            perc_rep_mat = (rep_words / doc_word_count) * 100
+            rep_mat_statment = f'     {round(perc_rep_mat, 2)} % repeated material.'
+            results['%'] = pd.Series(rep_mat_statment)
+            
+            
+            
+            
+            os.path.dirname(os.path.abspath(filename))
+            results.to_excel(filename + '_DOCX_RepStrings.xlsx')
+                
+            print("Results!")
+            print( "\n")
+            print(results[['String', 'No. of instances']])
+            print( "\n")
+            print(rep_mat_statment)  
+            print( "\n")
+            print("Your DOCX_RepStrings report was successfully created!")
